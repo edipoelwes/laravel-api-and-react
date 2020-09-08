d5012324a418275bcf4220e42a304ff6e598b9b9
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-09-08 13:09:16 -0300

    Api de produtos

---
+++ api/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $products = Product::all();
+
+    return response()->json($products);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $product = Product::create($request->all());
+
+    return response()->json([], 201);
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $product = Product::where('id', $id)->first();
+
+    return response()->json($product);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $product = Product::where('id', $id)->first();
+    $product->update($request->all());
+
+    return response()->json();
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $product = Product::find($id);
+    $product->delete();
+
+    return response()->json();
+  }
+}
